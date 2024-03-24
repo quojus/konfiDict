14343b63271cb1dc4be34599c1f05ea982d047c5
falk <malte.kaiser42@gmail.com>
2024-03-24 09:55:56 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,11 @@ setup(
     name='konfiDict',
     version='0.1.2',
     packages=find_packages(),
-    description='Eine dict mit besondern Fächkeiten',
+    description='Eine dict mit besonderen Fähigkeiten',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Falk',
     url='https://github.com/quojus/konfiDict',
     python_requires='>=3.6',
-    nstall_requires=[
-        # Liste von erforderlichen Paketen
-    ]
+
 )
